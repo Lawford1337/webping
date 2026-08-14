26267472ba201f893013024b583a25e689598bf5
OnijYT <jalolov.akmal001@gmail.com>
2026-08-14 19:05:27 +0500

    Monitors-ui: add header

--- next/app/(Dashboard)/monitors/page.tsx
+++ next/app/(Dashboard)/monitors/page.tsx
@@ -1,5 +1,19 @@
+'use client'
+import { Button } from "@/components/ui/button";
+
 export default function Monitors() {
     return (
-        <h1>Hello</h1>
+        <div className="flex flex-col ml-7 mr-7 mt-4 gap-10">
+            <div className="flex justify-between">
+                <div className="flex flex-col">
+                    <h1 className="text-[25px] ">Monitors</h1>
+                    <p className="text-[16px] ">Manage all your monitors</p>
+                </div>
+                <Button variant='secondary' size={'lg'} className="pl-5 pr-5 pt-5 pb-5 bg-[#54c167] hover:bg-[#54c167] hover:shadow-sm hover:shadow-green-500">+ Add Monitor</Button>
+            </div>
+            <div className="flex justify-between">
+                
+            </div>
+        </div>
     )
 }
