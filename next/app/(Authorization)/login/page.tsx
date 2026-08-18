4ca557a5dd8e9b9078988178f04697cabf67f7d2
OnijYT <jalolov.akmal001@gmail.com>
2026-08-18 17:52:53 +0500

    feat: implement Login component and create LoginPage for authentication

---
+++ next/app/(Authorization)/login/page.tsx
@@ -0,0 +1,7 @@
+import Login from "@/components/feature/Login";
+
+export default function LoginPage() {
+    return (
+        <Login />
+    )
+}
--- next/components/feature/Login.tsx
+++ next/components/feature/Login.tsx
@@ -1,8 +1,16 @@
 'use client'
-
+import { Activity } from "lucide-react"
 
 export default function Login() {
     return (
-        <h1></h1>
+        <div className="flex">            
+            {/* form register */}
+            <div className="flex flex-col">
+                <div className="flex gap-2 pt-3 pb-16 font-bold text-[20px]">
+                    <Activity color="#42a35e" />
+                    <h1 className="">Uptime Monitor</h1>
+                </div>
+            </div>
+        </div>
     )
 }
