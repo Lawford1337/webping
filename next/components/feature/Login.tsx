646be1e462cdd238053aef1bdec63dccbbaaf92c
OnijYT <jalolov.akmal001@gmail.com>
2026-08-20 01:14:15 +0500

    feat: enhance Login component with authentication icons and improved layout

--- next/components/feature/Login.tsx
+++ next/components/feature/Login.tsx
@@ -1,30 +1,61 @@
 'use client'
-import { Activity } from "lucide-react"
+import { Activity, LucideIcon, Zap, Shield, ChartNoAxesColumn } from "lucide-react"
+import Link from "next/link"
+
+interface IconsAuthForm {
+    title: string
+    bottomtitle: string
+    icon: LucideIcon
+    iconwidth: number
+}
+
+const IconAuth: IconsAuthForm[] = [
+    {title: 'Real-time Alerts', bottomtitle: 'Get notified instantly when something goes wrong.', icon: Zap, iconwidth:2,},
+    {title: 'Secure & Reliable', bottomtitle: 'Enterprise-grade security to keep your date safe.', icon: Shield, iconwidth: 2},
+    {title: 'Powerful Insights', bottomtitle: 'Track performance and uptime with detailed analytics.', icon: ChartNoAxesColumn, iconwidth: 3}
+]
 
 export default function Login() {
     return (
-        <div className="flex flex-1 bg-[#03070e] p-10">
+        <div className="flex flex-1 bg-[#03080d] p-10">
             {/* form register */}
-            <div className="flex flex-col">
-                <div className="flex flex-col">
+            <div className="flex-1 flex-col">
+                {/* header */}
+                <div className="flex justify-between">
                     <div className="flex gap-2 pt-3 pb-16 font-bold text-[20px]">
                         <Activity color="#42a35e" />
                         <h1 className="">Uptime Monitor</h1>
                     </div>
+                    <div className="flex gap-3">
+                        <p className="text-lg font-normal">Don't have an account?</p>
+                        <Link href={'/register'} className="font-normal text-lg text-auth-green">Sign up</Link>
+                    </div>
                 </div>
-                {/* left title */}
                 <div className="flex pt-5 pl-10 flex-col">
+                    {/* left title */}
                     <div className="flex justify-start mb-5">
                         <div className="flex justify-center border-1 border-[#039234] shadow-[0_0_5px] bg-[#050b10] shadow-green-900 rounded-xl pt-1 pb-1 pl-2 pr-4 ">
-                            <h2 className="tracking-widest text-[#01b134] text-xs">WELCOME BACK</h2>
+                            <h2 className="tracking-widest text-auth-green text-xs">WELCOME BACK</h2>
                         </div>
                     </div>
                     <div className="flex flex-col leading-none mb-5">
                         <h1 className="text-[50px] font-bold" >Log in to your</h1>
-                        <h1 className="text-[50px] font-bold text-[#03c34e] pb-6" >account</h1>
+                        <h1 className="text-[50px] font-bold text-auth-green pb-6" >account</h1>
                         <p className="font-normal text-[20px] leading-tight">Access oyur dashboard and continue <br /> monitoring your services in real-time.</p>
                     </div>
-                    <div className="h-[2px] w-64 bg-gradient-to-r from-green-400 to-transparent drop-shadow-[0_0_6px_rgba(16,185,129,0.9)]"></div>
+                    <div className="mb-10 h-[2px] w-64 bg-gradient-to-r from-green-400 to-transparent drop-shadow-[0_0_6px_rgba(16,185,129,0.9)]"></div>
+                    {/* info */}
+                    <div className="flex flex-col gap-5">
+                        {IconAuth.map(({title, bottomtitle, icon: Icon, iconwidth}) => (
+                            <div className="flex gap-3 items-center">
+                                <div className="flex justify-center items-center border-1 border-auth-green shadow-[0_0_4px_#039234] rounded-full p-4 bg-[#040b10]"><Icon strokeWidth={iconwidth} color="#03c34e" /></div>
+                                <div className="flex gap-3 flex-col leading-none justify-center">
+                                    <h2>{title}</h2>
+                                    <p className="ml-2 font-normal text-sm w-60">{bottomtitle}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>            
         </div>
