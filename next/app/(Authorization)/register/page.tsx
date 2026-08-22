411689337a3826dc34d5b43310b9c067564bb365
OnijYT <jalolov.akmal001@gmail.com>
2026-08-22 16:42:13 +0500

    feat: implement registration page with form validation and UI components

---
+++ next/app/(Authorization)/register/page.tsx
@@ -0,0 +1,10 @@
+import Register from "@/components/feature/Regiser";
+
+
+
+
+export default function RegisterPage() {
+    return(
+        <Register />
+    )
+}
--- next/components/feature/Regiser.tsx
+++ next/components/feature/Regiser.tsx
@@ -1,38 +1,49 @@
 'use client'
-import { Activity, LucideIcon, Zap, Shield, ChartNoAxesColumn, EyeOffIcon, Eye, Mail, } from "lucide-react"
+import { Activity, EyeOffIcon, Eye } from "lucide-react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Field, FieldLabel } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import { useState } from "react"
 import { InputGroup, InputGroupInput, InputGroupAddon } from "@/components/ui/input-group"
+import { IconAuth } from "./Login"
+import { useForm } from "react-hook-form"
+import { z } from "zod" 
+import { zodResolver } from "@hookform/resolvers/zod"
 
-interface IconsAuthForm {
-    title: string
-    bottomtitle: string
-    icon: LucideIcon
-    iconwidth: number
-}
+const registerschem = z
+    .object({
+        name: z
+            .string()
+            .min(4, 'Имя должно содержать минимум 4 буквы')
+            .regex(/^[a-zA-Zа-яА-ЯёЁuzUz'‘]+$/, "Имя должно состоять только из букв без пробелов и символов"),
+        email: z.string().email('Некоректый email'),
+        password: z.string().min(8, 'Пароль должен быть минимум 8 символов'),
+        confirmpassword: z.string().min(1, 'подвердите пароль'),
+    })
+    .refine((data) => data.password === data.confirmpassword, {
+        message: 'Пароль не совпадает',
+        // path тут для того что бы оперделил путь для errors что бы так вызывать errors.ConfirmPassword
+        path: ['ConfirmPassword'],
+    })
 
-export const IconAuth: IconsAuthForm[] = [
-    {title: 'Real-time Alerts', bottomtitle: 'Get notified instantly when something goes wrong.', icon: Zap, iconwidth:2,},
-    {title: 'Secure & Reliable', bottomtitle: 'Enterprise-grade security to keep your date safe.', icon: Shield, iconwidth: 2},
-    {title: 'Powerful Insights', bottomtitle: 'Track performance and uptime with detailed analytics.', icon: ChartNoAxesColumn, iconwidth: 3}
-]
+    // тут я использую z.infer что бы взять структуру схемы
+    type RegisterForm = z.infer<typeof registerschem>
 
-export default function Login() {
+export default function Register() {
     const [showpass, setShowpass] = useState(false)
+    const {register, handleSubmit, formState: { errors }} = useForm<RegisterForm>({resolver: zodResolver(registerschem)})
+
+    const registersubmit = async (data: RegisterForm) => {
+        console.log(data)
+    }
     return (
         <div className="flex flex-col flex-1 bg-[#03080d] p-10">
             {/* header */}
-            <div className="flex justify-between">
+            <div className="flex justify-start">
                 <div className="flex gap-2 pt-3 pb-16 font-bold text-[20px]">
                     <Activity color="#42a35e" />
                     <h1 className="">Uptime Monitor</h1>
-                </div>
-                <div className="flex gap-3">
-                    <p className="text-lg font-normal">Don't have an account?</p>
-                    <Link href={'/register'} className="font-normal text-lg text-auth-green">Sign up</Link>
                 </div>
             </div>
             {/* form register */}
@@ -42,13 +53,13 @@ export default function Login() {
                         {/* left title */}
                         <div className="flex justify-start mb-5">
                             <div className="flex justify-center border-1 border-[#039234] shadow-[0_0_5px] bg-[#050b10] shadow-green-900 rounded-xl pt-1 pb-1 pl-2 pr-4 ">
-                                <h2 className="tracking-widest text-auth-green text-xs">WELCOME BACK</h2>
+                                <h2 className="tracking-widest text-auth-green text-sm">Let's get started</h2>
                             </div>
                         </div>
                         <div className="flex flex-col leading-none mb-5">
-                            <h1 className="text-[50px] font-bold" >Log in to your</h1>
+                            <h1 className="text-[50px] font-bold" >Create your</h1>
                             <h1 className="text-[50px] font-bold text-auth-green pb-6" >account</h1>
-                            <p className="font-normal text-[20px] leading-tight">Access oyur dashboard and continue <br /> monitoring your services in real-time.</p>
+                            <p className="font-normal text-[20px] leading-tight">Join Uptime Monitor and start <br /> monitoring your services in real-time.</p>
                         </div>
                         <div className="mb-10 h-[2px] w-64 bg-gradient-to-r from-green-400 to-transparent drop-shadow-[0_0_6px_rgba(16,185,129,0.9)]"></div>
                         {/* info */}
@@ -70,11 +81,26 @@ export default function Login() {
                     <div className="flex flex-2 flex-col gap-8 p-12 rounded-2xl border-1 border-transparent bg-origin-border [background-clip:padding-box,border-box] [background-image:linear-gradient(#070d13,#070d13),linear-gradient(to_top_right,#141d27_50%,#03c34e_100%)] [box-shadow:12px_-12px_35px_-5px_rgba(3,195,78,0.4)]">
                         {/* title */}
                         <div className="flex flex-col text-start gap-3">
-                            <h1 className="text-2xl">Welcome back!</h1>
-                            <p className="text-sm font-normal">Log in to your account to continue</p>
+                            <h1 className="text-2xl">Let's get started</h1>
+                            <p className="text-sm font-normal">Fill in the details below to create your account</p>
                         </div>
                         {/* email & pass */}
-                        <div className="">
+                        <form onSubmit={handleSubmit(registersubmit)} className="flex flex-col gap-8">
+                            <Field>
+                                <FieldLabel htmlFor="inline-end-input">name</FieldLabel>
+                                <InputGroup className="border-2 p-5">
+                                  <InputGroupInput
+                                    {...register('name', {required: true})}
+                                    id="inline-end-input"
+                                    className=""
+                                    type='text'
+                                    placeholder="John"
+                                  />
+                                </InputGroup>
+                                {errors.name && (
+                                    <p className="mt-1 text-xs text-red-500">{errors.name.message}</p>
+                                  )}
+                            </Field>
                             <Field>
                                 <FieldLabel htmlFor="input-field-email">Email</FieldLabel>
                                 <Input
@@ -84,9 +110,7 @@ export default function Login() {
                                   placeholder="john@example.com"
                                 />
                             </Field>
-                        </div>
-                        <div className="">
-                            <Field className="">
+                            <Field>
                                 <FieldLabel htmlFor="inline-end-input">Password</FieldLabel>
                                 <InputGroup className="border-2 p-5">
                                   <InputGroupInput
@@ -100,24 +124,40 @@ export default function Login() {
                                   </InputGroupAddon>
                                 </InputGroup>
                             </Field>
-                        </div>
-                        {/* remember me */}
-                        <div className="flex justify-start">
-                            <div className="">
-                                <input className="" type='checkbox' /> Remember me
+                            <Field>
+                                <FieldLabel htmlFor="inline-end-input">Confirm Password</FieldLabel>
+                                <InputGroup className="border-2 p-5">
+                                  <InputGroupInput
+                                    id="inline-end-input"
+                                    className=""
+                                    type={showpass ? "password" : 'text'}
+                                    placeholder="Enter password"
+                                  />
+                                  <InputGroupAddon onClick={() => setShowpass(!showpass)} align="inline-end">
+                                    {showpass ? <EyeOffIcon /> : <Eye />}
+                                  </InputGroupAddon>
+                                </InputGroup>
+                            </Field>
+                        
+                        
+                            {/* remember me */}
+                            <div className="flex justify-start">
+                                <div className="">
+                                    <input className="" type='checkbox' /> Remember me
+                                </div>
                             </div>
-                        </div>
-                        <Button className={'bg-auth-green hover:bg-auth-green p-7 text-lg text-white shadow-[0_0_4px_#028837] border-none hover:shadow-[0_0_10px_#028837] '}>Log In</Button>
+                            <Button type='submit' className={'bg-auth-green hover:bg-auth-green p-7 text-lg text-white shadow-[0_0_4px_#028837] border-none hover:shadow-[0_0_10px_#028837] '}>
+                                Create Account
+                            </Button>
+                        </form>
                         <div className="flex gap-5 items-center">
                             <div className="bg-[#141b22] rounded-lg h-1 w-full"></div>
                             <p>or</p>
                             <div className="bg-[#141b22] rounded-lg h-1 w-full"></div>
                         </div>
-                        <div className="flex justify-center items-center rounded-lg border-2 border-[#1d242d] p-4">
-                            <div className="flex gap-3 justify-center items-center text-lg">
-                                <Mail size={25} color="#03c34e"/>
-                                <h3 className="">Continue with Mail</h3>
-                            </div>
+                        <div className="flex gap-2 justify-center items-center text-lg">
+                            <h3 className="text-lg font-normal">Already have an account?</h3>
+                            <Link href={'/login'} className="font-normal text-lg text-auth-green">Sign in</Link>
                         </div>
                     </div>
                 </div>
